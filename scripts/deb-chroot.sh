c1ccd95d19662b19cc4d6b43b2e4b70c333d87c2
Joel Steres <joel.steres@ymobility.com>
2013-03-08 10:15:30 -0800

    Notes and deb-chroot.sh changes that were left uncommited on zhen related to setting up nameserver and mounting into the chroot.

--- scripts/deb-chroot.sh
+++ scripts/deb-chroot.sh
@@ -1,9 +1,14 @@
-DIR=/mnt/sdc1/expire/chroot
+if [ -z $1 ]; then
+    DIR=/knowledge/processed/chroot-armel
+else
+    DIR="$1"
+fi
+echo "DIR=$DIR"
 mount -t proc none ${DIR}/proc
 mount -t sysfs none ${DIR}/sys
 mount -o bind /dev ${DIR}/dev
 mount -o bind /dev/pts ${DIR}/dev/pts # important for pacman (for signature check)
 mkdir ${DIR}/knowledge
-mount -o bind /public2/knowledge ${DIR}/knowledge
-mount -o bind /var/lib/mod_tile ${DIR}/var/lib/mod_tile
+mount -o bind /knowledge ${DIR}/knowledge
+#mount -o bind /var/lib/mod_tile ${DIR}/var/lib/mod_tile
 chroot ${DIR}
