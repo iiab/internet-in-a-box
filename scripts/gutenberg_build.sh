d7ef948c6599c937ece6918b511ef9723bed283e
Joel Steres <joel@ymobility.com>
2013-03-08 02:53:06 +0000

    cleanup gutenberg_build script.

--- scripts/gutenberg_build.sh
+++ scripts/gutenberg_build.sh
@@ -3,21 +3,22 @@
 # abort script on error
 set -e
 
-echo Building database from gutenberg RDF XML index
 CATALOG=../catalog.rdf.bz2
 DBNAME=../gutenberg.db
+WHOOSH_DIR=../whoosh/gutenberg_index
+MODEL=../iiab/gutenberg_models.py
+
+echo Building database from gutenberg RDF XML index
 ./gutenberg_db_build.py --rdfindex $CATALOG --dbname $DBNAME
 
 echo Building whoosh index
-WHOOSH_DIR=../whoosh/gutenberg_index
 ./gutenberg_whoosh_build.py --db $DBNAME --indexdir $WHOOSH_DIR
 
 echo Creating SQL object model
-MODEL=../iiab/gutenberg_models.py
 # if sqlautocode is not found, you may need to pip install sqlautocode
 # Option -d means use declarative object model format.
 # sed script post processes to produce result for use with flask-sqlalchemy
-sqlautocode -d sqlite:///../gutenberg.db | sed -f model_fixup.sed > $MODEL
+sqlautocode -d sqlite:///${DBNAME} | sed -f model_fixup.sed > $MODEL
 
 
 
