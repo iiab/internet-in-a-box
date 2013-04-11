bfd483d691a9de740e5cadef6a1c168873a347ec
Omwah <omwah@towhee.org>
2013-04-10 19:30:48 -0700

    Initial addition of Pelican based website

---
+++ website/pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Braddock Gaskill'
+SITENAME = u'Internet-in-a-Box'
+SITEURL = ''
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Custom theme based on Github Pages slate theme
+THEME = "theme/slate"
