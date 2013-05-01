1a14b6acc29658eeb897c5bcb2c4b4a9d6d82f2c
Braddock Gaskill <braddock@braddock.com>
2013-05-01 19:52:17 +0000

    startup scripts, tweaks

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 
 import sys
 from optparse import OptionParser
+import os
 
 sys.path.append('.')
 
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "Hello World!"
 killall lighttpd-angel
-(cd /knowledge/internet-in-a-box; ./run.py) &
-(cd /knowledge/modules/wikipedia-kiwix; /root/repo/kiwix-kiwix/src/server/kiwix-serve -v --library library.xml --port 25001) &
+(cd /knowledge/internet-in-a-box; ./run.py 2>&1 >>/knowledge/sys/iiab.log) &
+(cd /knowledge/modules/wikipedia-kiwix; /knowledge/sys/bin-arm/kiwix-serve-patched -v --library library.xml --port 25001 2>&1 >>/knowledge/sys/kiwix.log) &
 /etc/init.d/nginx start
 nginx -s reload
 echo "Ready to serve."
-bash
