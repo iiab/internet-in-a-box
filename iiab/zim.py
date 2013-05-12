455b29b2cd5df25b33cf14abb9a12bcef83f6993
Braddock Gaskill <braddock@braddock.com>
2013-05-12 09:08:38 -0700

    new zimdump wikipedia loading without kiwix

--- iiab/zim.py
+++ iiab/zim.py
@@ -1,3 +1,7 @@
+# Extract articles from ZIM files using zimdump (for now)
+# Must use a patched version of zimdump
+# By Braddock Gaskill May 2013
+
 from subprocess import Popen, PIPE
 import os
 import re
@@ -7,35 +11,148 @@ import kiwix
 from config import config
 
 
-def get_article_by_url(zimfile, namespace, url, cwd='.'):
-    name = namespace + "/" + url
+def parse_zimdump_line(line):
+    line = line.strip()
+    g = re.match("([a-z\- ]+): +(.+)", line)
+    if g is not None:
+        return g.groups()[0], g.groups()[1]
+    else:
+        return None, None
+
+
+def parse_zimdump_info(text):
+    d = {}
+    lines = text.split("\n")
+    for line in lines:
+        print line
+        name, value = parse_zimdump_line(line)
+        if name is not None:
+            d[name] = value
+    return d
+
+
+def get_article_info_by_url(zimfile, namespace, url, cwd='.'):
+    name = "X" + namespace + "/" + url
     cmd = ['zimdump',
-           '-d', '-u', name,
+           '-i', '-u', name,
            zimfile]
     print string.join(cmd, " ")
     p = Popen(cmd, stdout=PIPE, cwd=cwd)
     data = p.stdout.read()
     if len(data) == 0:
         print "ZERO LENGTH FILE"
-    data = data.decode('utf-8')
+        return None
+    return parse_zimdump_info(data)
+
+
+def get_zimfile_info(zimfile, cwd='.'):
+    """Return info about the zim file itself"""
+    cmd = ['zimdump',
+           '-F', '-v',
+           zimfile]
+    print string.join(cmd, " ")
+    p = Popen(cmd, stdout=PIPE, cwd=cwd)
+    data = p.stdout.read()
+    info = parse_zimdump_info(data)
+    return info
+
+
+def get_article_info_by_index(zimfile, idx, cwd='.'):
+    cmd = ['zimdump',
+           '-i', '-o', str(idx),
+           zimfile]
+    print string.join(cmd, " ")
+    p = Popen(cmd, stdout=PIPE, cwd=cwd)
+    data = p.stdout.read()
+    if len(data) == 0:
+        print "ZERO LENGTH FILE"
+        return None
+    return parse_zimdump_info(data)
+
+
+def get_article_data_by_index(zimfile, index, cwd='.'):
+    cmd = ['zimdump',
+           '-d', '-o', str(index),
+           zimfile]
+    print string.join(cmd, " ")
+    p = Popen(cmd, stdout=PIPE, cwd=cwd)
+    data = p.stdout.read()
+    if len(data) == 0:
+        print "ZERO LENGTH FILE"
     return data
+
+
+def get_article_by_url(zimfile, namespace, url, cwd='.'):
+    info = get_article_info_by_url(zimfile, namespace, url, cwd=cwd)
+    if not bool(int(info.get('redirect'))):
+        data = get_article_data_by_index(zimfile, info['idx'], cwd=cwd)
+    else:
+        data = None
+    return data, info
+
+
+def get_article_by_index(zimfile, idx, cwd='.'):
+    info = get_article_info_by_index(zimfile, idx, cwd=cwd)
+    if not bool(int(info.get('redirect'))):
+        data = get_article_data_by_index(zimfile, info['idx'], cwd=cwd)
+    else:
+        data = None
+    return data, info
 
 
 def replace_paths(top_url, html):
     replace = u"\\1\\2" + top_url + "/\\3/"
-    html = re.sub(  u'(href|src)(=["\']/)([A-Z\-])/', replace, html)
+    html = re.sub(u'(href|src)(=["\']/)([A-Z\-])/', replace, html)
     html = re.sub(u'(@import[ ]+)(["\']/)([A-Z\-])/', replace, html)
     return html
+
+
+class ZimArticle(object):
+    def __init__(self, data, info):
+        self.data = data
+        self.url = info['url']
+        self.title = info['title']
+        self.idx = int(info['idx'])
+        self.namespace = info['namespace']
+        self.redirect = bool(int(info['redirect']))
+        self.mime_type = info.get('mime-type', None)
+        self.size = info.get('article size', None)
+        self.redirect_index = info.get('redirect index', None)
 
 
 class ZimFile(object):
     def __init__(self, book):
         self.book = book
 
-    def get_article_by_url(self, namespace, url):
+    def get_filename(self):
         kiwix_dir = config().get('KIWIX', 'wikipedia_kiwix_dir')
         path = os.path.join(kiwix_dir, self.book['path'])
-        return get_article_by_url(path, namespace, url, cwd=kiwix_dir)
+        return path
+
+    def get_article_by_url(self, namespace, url):
+        kiwix_dir = config().get('KIWIX', 'wikipedia_kiwix_dir')
+        path = self.get_filename()
+        data, info = get_article_by_url(path, namespace, url, cwd=kiwix_dir)
+        article = ZimArticle(data, info)
+        while article.redirect:
+            data, info = get_article_by_index(path, article.redirect_index, cwd=kiwix_dir)
+            article = ZimArticle(data, info)
+        return article
+
+    def get_article_by_index(self, idx):
+        kiwix_dir = config().get('KIWIX', 'wikipedia_kiwix_dir')
+        path = self.get_filename()
+        data, info = get_article_by_index(path, idx, cwd=kiwix_dir)
+        article = ZimArticle(data, info)
+        while article.redirect:
+            data, info = get_article_by_index(path, article.redirect_index, cwd=kiwix_dir)
+            article = ZimArticle(data, info)
+        return article
+
+    def get_info(self):
+        kiwix_dir = config().get('KIWIX', 'wikipedia_kiwix_dir')
+        info = get_zimfile_info(self.get_filename(), cwd=kiwix_dir)
+        return info
 
 
 class Library(object):
@@ -45,8 +162,11 @@ class Library(object):
 
     def get_article_by_url(self, humanReadableId, namespace, url):
         print "URL: " + namespace + "/" + url
-        zimfile = self.readableToBooks.get(humanReadableId, None)
+        zimfile = self.get_zimfile(humanReadableId)
         if zimfile is None:
             return None
-        data = zimfile.get_article_by_url(namespace, url)
-        return data
+        article = zimfile.get_article_by_url(namespace, url)
+        return article
+
+    def get_zimfile(self, humanReadableId):
+        return self.readableToBooks.get(humanReadableId, None)
--- iiab/zim_views.py
+++ iiab/zim_views.py
@@ -1,4 +1,4 @@
-# OpenStreetMap URL views
+# ZIM file URL views (for Wikipedia)
 from flask import Blueprint, Response
 
 from zim import Library, replace_paths
@@ -8,10 +8,36 @@ blueprint = Blueprint('zim_views', __name__,
                       template_folder='templates', static_folder='static')
 
 
+@blueprint.route('/<humanReadableId>')
+def zim_main_page_view(humanReadableId):
+    """Returns the main page of the zim file"""
+    library_xml = config().get('KIWIX', 'library')
+    lib = Library(library_xml)
+    zimfile = lib.get_zimfile(humanReadableId)
+    info = zimfile.get_info()
+    article = zimfile.get_article_by_index(info.get('main page', 1))
+    html = mangle_article(article, humanReadableId)
+    return Response(html, mimetype=article.mime_type)
+
+
+def mangle_article(article, humanReadableId):
+    html = article.data
+    if article.mime_type in ['text/html; charset=utf-8', 'stylesheet/css']:
+        try:
+            html = html.decode('utf-8')
+        except UnicodeDecodeError:
+            try:
+                html = html.decode('latin1')
+            except:
+                return html
+        html = replace_paths("iiab/zim/" + humanReadableId, html)
+    return html
+
+
 @blueprint.route('/<humanReadableId>/<namespace>/<path:url>')
 def zim_view(humanReadableId, namespace, url):
     library_xml = config().get('KIWIX', 'library')
     lib = Library(library_xml)
-    data = lib.get_article_by_url(humanReadableId, namespace, url)
-    data = replace_paths("iiab/zim/" + humanReadableId, data)
-    return data
+    article = lib.get_article_by_url(humanReadableId, namespace, url)
+    html = mangle_article(article, humanReadableId)
+    return Response(html, mimetype=article.mime_type)
