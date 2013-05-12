b33afec35b3ed8247e5e695cf55ecaedda666823
root <root@xo-d2-7d-87.localdomain>
2013-05-12 16:43:42 +0000

    fixed the gutenberg data link

--- scripts/startup-xo4.sh
+++ scripts/startup-xo4.sh
@@ -3,5 +3,5 @@ chmod a+x /run/media/olpc/
 mkdir /var/log/nginx
 nginx
 (cd /knowledge/internet-in-a-box; ./run.py 2>&1 >>/knowledge/sys/iiab.log) &
-(cd /knowledge/modules/wikipedia-kiwix; /knowledge/sys/bin-arm/kiwix-serve -v --library library.xml --port 25001 2>&1 >>/knowledge/sys/kiwix.log) &
+#(cd /knowledge/modules/wikipedia-kiwix; /knowledge/sys/bin-arm/kiwix-serve -v --library library.xml --port 25001 2>&1 >>/knowledge/sys/kiwix.log) &
 echo "Ready to serve."
