1bff9e12213fc6e57dab790b74c92b4b76ac932b
Braddock Gaskill <braddock@braddock.com>
2013-06-06 16:29:37 -0700

    Added khanlinks served from Flask

--- iiab/top_views.py
+++ iiab/top_views.py
@@ -1,5 +1,6 @@
 # Top level URL views
-from flask import Blueprint, Response, request, redirect, make_response, render_template, send_file
+import os
+from flask import Blueprint, make_response, render_template, send_file, send_from_directory
 
 from config import config
 
@@ -16,3 +17,12 @@ def index():
 def test():
     print "TEST"
     return make_response((send_file('/var/www/foo.webm', mimetype="video/webm"), 200, {'Accept-Ranges': 'bytes'}))
+
+
+@blueprint.route('khanvideo/<path:filename>')
+def khanvideo_view(filename):
+    khanacademy_dir = config().get('VIDEO', 'khanacademy_dir')
+    khanlinks = os.path.join(khanacademy_dir, 'khanlinks')
+    print "khanlinks = ", khanlinks, " filename = ", filename
+    r = send_from_directory(khanlinks, filename)
+    return make_response(r, 200, {'Accept-Ranges': 'bytes'})
