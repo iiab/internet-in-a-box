62a013aceba990163ac48f0e0e91a531cdc2889d
Braddock Gaskill <braddock@braddock.com>
2013-06-22 11:54:32 -0700

    Improved RPM build script rsync call.  Added QuickStart sampler build script

---
+++ scripts/build_sampler.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+TEMP=`tempfile`
+if [ ! -x modules ]; then
+    echo "Must run from /knowledge directory"
+    exit -1
+fi
+
+find modules/openstreetmap/mod_tile64/default/{0,1,2,3,4,5,6,7,8} >>$TEMP
+find modules/gutenberg >>$TEMP
+find modules/wikipedia-zim/wikipedia_gn_all_01_2013.zim >>$TEMP
+find "modules/khanacademy/webm/Art History/01 Introduction to Art History - 02 Media"* >>$TEMP
+find "modules/khanacademy/h264/Art History/01 Introduction to Art History - 02 Media"* >>$TEMP
+find modules/wikipedia-kiwix/library.xml >>$TEMP
+find modules/wikipedia-kiwix/index/wikipedia_gn_all_01_2013/ >>$TEMP
+find modules/INFO.json modules/*/INFO.json >>$TEMP
+
+# Moby Dick
+find modules/gutenberg-mirror/data/2/7/0/2701 >>$TEMP
+
+# Bash software
+find modules/ubuntu/12.04/mirror/archive.ubuntu.com/ubuntu/pool/main/b/bash >>$TEMP
+
+if [ -x sampler/knowledge ]; then
+    echo "Deleting existing sampler/knowledge directory..."
+    rm -rf sampler/knowledge
+fi
+mkdir -p sampler/knowledge
+
+echo "Copying files to sampler/..."
+tar cBf - -T $TEMP |(cd sampler/knowledge; tar xBf -)
+
+grep -e '<library' -e '<?xml' -e wikipedia_gn_all_01_2013 -e '</library>' modules/wikipedia-kiwix/library.xml >sampler/knowledge/modules/wikipedia-kiwix/library.xml
+
+NAME=IIAB_QuickStart_Sampler_`date +%Y%m%d`.tgz
+echo "Creating tarball $NAME..."
+tar czf "$NAME" -C sampler knowledge
+echo "Complete"
