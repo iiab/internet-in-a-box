230942dbb566f76441f3c80a9f203afe6c070ee6
braddock <braddock@localhost.localdomain>
2013-07-01 15:18:53 -0400

    added RPM requirement for backports.lzma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Internet-in-a-Box',
-    version='0.3.8',
+    version='0.3.9',
     author="Braddock Gaskill",
     author_email="braddock@braddock.com",
     license="2-clause BSD license",
